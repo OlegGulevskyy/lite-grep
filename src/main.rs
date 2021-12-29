cffade8dbc48c9486abf42ac9d72f91ed3a65a95
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2021-12-29 11:29:58 +0100

    add UserArgs struct + file path

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use regex::Regex;
-use tests::{get_search_results, get_user_defined_pattern, print_results_to_console};
+use tests::{get_search_results, get_user_arguments, print_results_to_console};
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
 fn main() {
+	let user_arguments = get_user_arguments();
+	let re = Regex::new(&user_arguments.search_pattern).unwrap();
+	let file_path = user_arguments.file_path;
 	
-	let pattern = get_user_defined_pattern();
-	let re = Regex::new(&pattern).unwrap();
-
-	let file = File::open("readme.md").unwrap();
+	let file = File::open(file_path).unwrap();
 	let reader = BufReader::new(file);
 	let lines: Vec<String> = reader.lines().map(|f|f.unwrap()).collect();
 
